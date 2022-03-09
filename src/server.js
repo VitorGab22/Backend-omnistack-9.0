8a522b360a02437c9557af553a9daa16afb2f441
Vitor Gabriel <vitor.gabriel@amppli.com.br>
2022-03-09 13:55:44 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log('Usuário conectado', socket.id);
+
+  socket.emit('hello', 'world');
+});
+
+mongoose.connect('mongodb+srv://Vitorstack:gabriel2014@cluster0.zuj81.mongodb.net/backend?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+//GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição,delete)
+//req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
